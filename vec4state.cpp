a1259d2f0ed0c5c911c2a99ab80347d8776d3e34
yuval-shasha <yuvalshasha11@gmail.com>
2024-07-23 22:00:14 +0300

    added C'TOR with long a parameter

--- vec4state.cpp
+++ vec4state.cpp
@@ -9,7 +9,19 @@ vec4state::vec4state()
 vec4state::vec4state(int num)
 {
     vector = new VPI[1];
-    size = 1;
+    size = 32;
     vector[0].setAval(num);
     vector[0].setBval(0);
 }
+
+vec4state::vec4state(long num)
+{
+    vector = new VPI[2];
+    size = 64;
+    long mask = 0xFFFFFFFF;
+    vector[0].setAval(num & mask);
+    vector[1].setAval(num >> 32);
+    vector[0].setBval(0);
+    vector[1].setBval(0);
+}
+
