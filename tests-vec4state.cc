7d3ed0e0e1a8226f31be1d793eb3267b0b655e24
Mia Ekheizer <miaekh12@gmail.com>
2024-08-01 20:15:34 +0300

    added assignment operators tests, passed them as well

--- tests-vec4state.cc
+++ tests-vec4state.cc
@@ -52,3 +52,25 @@ TEST_F(vec4stateTest, TestConstructors) {
     EXPECT_TRUE(compareVectorToString(copyVector, string("01xz11")));
     EXPECT_TRUE(checkVectorSize(copyVector, 6));
 }
+
+TEST_F(vec4stateTest, TestAssignmentOperators) {
+    // Copy assignment
+    vec4state copy = defaultVector;
+    EXPECT_TRUE(compareVectorToString(copy, string("x")));
+    EXPECT_TRUE(checkVectorSize(copy, 1));
+
+    // Integer assignment
+    copy = 0x12345678;
+    EXPECT_TRUE(compareVectorToString(copy, string("00010010001101000101011001111000")));
+    EXPECT_TRUE(checkVectorSize(copy, 32));
+
+    // Long long assignment
+    copy = 0x1234567890ABCDEF;
+    EXPECT_TRUE(compareVectorToString(copy, string("0001001000110100010101100111100010010000101010111100110111101111")));
+    EXPECT_TRUE(checkVectorSize(copy, 64));
+
+    // String assignment
+    copy = "01xz11";
+    EXPECT_TRUE(compareVectorToString(copy, string("01xz11")));
+    EXPECT_TRUE(checkVectorSize(copy, 6));
+}
